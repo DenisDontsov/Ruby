87b0dbf2232985c845518da12623ae121900d385
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-21 20:50:43 +0300

    Commit

---
+++ ЛР 2/p2-6-16.rb
@@ -0,0 +1,23 @@
+
+puts "Введите элементы целочисленного массива (пустая строка завершает ввод):"
+a = []
+input = STDIN.gets.chomp
+while input != ""
+    a.concat(input.split.map{ |i| Integer(i)})
+    input = STDIN.gets.chomp
+end
+i1 = 0
+i2 = 0
+for i in 0...a.length
+    if a[i] == a.max
+        i1 = i
+        break
+    end
+end
+for i in i1+1...a.length
+    if a[i] == a.max
+        i2 = i
+        break
+    end
+end
+print a.slice!(i1..i2)
