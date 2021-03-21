3ea983171151320121e952108457d529dda790d8
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-21 21:24:26 +0300

    Commit

---
+++ ЛР 2/p2-6-40.rb
@@ -0,0 +1,8 @@
+puts "Введите элементы целочисленного массива (пустая строка завершает ввод):"
+a = []
+input = STDIN.gets.chomp
+while input != ""
+    a.concat(input.split.map{ |i| Integer(i)})
+    input = STDIN.gets.chomp
+end
+print (a.filter { |i| i.even?}).min
