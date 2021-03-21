c077bb0b97c6d820f7fd0df7e980bd8d8d03421b
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-21 22:02:49 +0300

    Commit

--- ЛР 2/p2-6-16.rb
+++ ЛР 2/p2-6-16.rb
@@ -5,18 +5,5 @@ while input != ""
     a.concat(input.split.map{ |i| Integer(i)})
     input = STDIN.gets.chomp
 end
-i1 = 0
-i2 = 0
-for i in 0...a.length
-    if a[i] == a.max
-        i1 = i
-        break
-    end
-end
-for i in i1+1...a.length
-    if a[i] == a.max
-        i2 = i
-        break
-    end
-end
-print a.slice!(i1..i2)
+puts b = a.slice(((a.index(a.max)) + 1)..a.length)
+print a.slice!(a.index(a.max)..(a.index(a.max) + b.index(a.max) + 1))
