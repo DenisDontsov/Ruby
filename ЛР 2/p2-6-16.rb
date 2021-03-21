79edb3c143ceac8829f67f06d953f1d3805f0999
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-21 21:03:54 +0300

    Commit

--- ЛР 2/p2-6-16.rb
+++ ЛР 2/p2-6-16.rb
@@ -10,13 +10,11 @@ i2 = 0
 for i in 0...a.length
     if a[i] == a.max
         i1 = i
-        break
     end
 end
 for i in i1+1...a.length
     if a[i] == a.max
         i2 = i
-        break
     end
 end
 print a.slice!(i1..i2)
