7fe0a21fcb92d1e38f1627365be73713d4df9a1b
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-21 18:35:48 +0300

    Commit

---
+++ ЛР 2/p2-1.rb
@@ -0,0 +1,39 @@
+def min array
+    min = array[0]
+    for item in array
+        min = item if item < min
+    end
+    min
+end
+def max array
+    max = array[0]
+    for item in array
+        max = item if item > max
+    end
+    max
+end
+def sum array
+    sum = 0
+    for item in array
+        sum += item
+    end
+    sum
+end
+def product array
+    product = 1
+    for item in array
+        product *= item
+    end
+    product
+end
+puts "Введите элементы списка (пустая строка завершает ввод):"
+a = Array.new
+input = STDIN.gets.chomp
+while input != ""
+    a.concat(input.split.map{ |i| Integer(i)})
+    input = STDIN.gets.chomp
+end
+puts "Минимум - #{min a}"
+puts "Максимум - #{max a}"
+puts "Сумма - #{sum a}"
+puts "Произведение - #{product a}"
