f3dc5294984a944bae947b74481fa64aaba4e264
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-24 13:28:46 +0300

    Commit

---
+++ ЛР 1/p1-7-4-3.rb
@@ -0,0 +1,29 @@
+def lcd a
+    b = 2
+    while a % b != 0
+        b += 1
+    end
+    b
+end
+
+def max_coprime a
+    b = a - 1
+    while (b >= 2) ^ ((gcd(a, b) == 1) ^ (b % lcd(a) == 1))
+        b -= 1
+    end
+    b
+end
+
+def sum_of_digits number_string
+    sum = 0
+    number_string.each_char do |i|
+        sum += Integer(i) if Integer(i) < 5
+    end
+    sum
+end
+
+puts "Введите число"
+x = Integer(STDIN.gets.chomp)
+puts "Произведение максимального числа, не взаимно простого с данным, 
+не делящегося на наименьший делитель исходно числа, 
+и суммы цифр числа, меньших 5 равно #{max_coprime(x) * sum_of_digits(x.to_s)}"
