b0b6a7c53ac8a992f2fa085ae211fd931f94b207
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-24 14:39:44 +0300

    Commit

--- ЛР 2/p2-10.rb
+++ ЛР 2/p2-10.rb
@@ -1,3 +1,18 @@
-puts "Введите строку"
-str = STDIN.gets.chomp
-str.scan(/\s+(\d\d?\s+(?:декабря|января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября)\s+\d{1,4})/ixu)
+# Чтение файла
+def readFromFile(filePath)
+	if not File.file?(filePath)
+		return nil
+	end
+    file = File.new(filePath, "r:UTF-8")
+    lines = file.readlines
+	return lines.join(" ")
+end
+
+print "Введите путь к файлу с исходным текстом: "
+filePath = STDIN.gets.chomp.strip
+text = readFromFile(filePath)
+if text == nil
+    puts "Файл пуст, либо не найден."
+else
+    puts text.scan(/\s?+(\d\d?\s+(?:декабря|января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября)\s+\d{1,4})/ixu)
+end
