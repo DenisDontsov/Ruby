3f57f2dc5a1d38dfe8e681c0d312532e7434c669
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-25 15:16:46 +0300

    Commit

--- ЛР 1/p1-3.rb
+++ ЛР 1/p1-3.rb
@@ -1,13 +1,23 @@
 puts "Привет, какой твой любимый язык программирования?"
 favourite_language = STDIN.gets.chomp()
 
-if favourite_language == "Ruby" then
-    puts "Пользователь есть подлиза"
+if favourite_language == "Ruby"
+    puts "Пользователь есть подлиза"    
+elsif favourite_language == "Assembler"
+    puts "Вот точно будет Ruby"
 else
     puts "Cкоро будет ruby"
 end
 
 puts "Cкоро будет ruby" if favourite_language == "C++"
+
+unless favourite_language != "Ruby"
+    puts "Пользователь есть подлиза"    
+elsif favourite_language == "Assembler"
+    puts "Вот точно будет Ruby"
+else
+    puts "Cкоро будет ruby"
+end
 
 case favourite_language
 when "Ruby"
