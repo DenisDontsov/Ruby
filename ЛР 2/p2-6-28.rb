1315125f2d1827c13c45c074f2ce499fa21926eb
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-26 09:16:18 +0300

    Commit

--- ЛР 2/p2-6-28.rb
+++ ЛР 2/p2-6-28.rb
@@ -1,8 +1,48 @@
-puts "Введите элементы целочисленного массива (пустая строка завершает ввод):"
-a = []
-input = STDIN.gets.chomp
-while input != ""
-    a.concat(input.split.map{ |i| Integer(i)})
-    input = STDIN.gets.chomp
+def main
+    puts "Введите количество элементов"
+    n = Integer STDIN.gets.chomp
+    puts "Введите список"
+    list = []
+    for i in 0...n
+        item = Integer STDIN.gets.chomp
+        list[i] = item
+    end
+    min = list[0]
+    for i in 0...n
+        if min > list[i]
+            min = list[i]
+        end
+    end
+    max1 = min
+    max1_i = -1
+    max2 = min
+    max2_i = -1
+    i = n - 1
+    while (i >= 0)
+        if list[i] >= max1
+            max1 = list[i]
+            max1_i = i
+        end
+        i -= 1
+    end
+    i = 0
+    while (i < n)
+        if (list[i] >= max2) && (i != max1_i)
+            max2 = list[i]
+            max2_i = i
+        end
+        i += 1
+    end
+    if max1_i < max2_i
+        for i in max1_i..max2_i
+            print "#{list[i]} "
+        end
+    elsif max1_i > max2_i
+        for i in max2_i..max1_i
+            print "#{list[i]} "
+        end
+    else
+        print"#{list[max1_i]} #{list[max2_i]}"
+    end
 end
-print a.slice!(a.index(a.max)..a.rindex(a.max))
+main
