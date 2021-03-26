3d2e46a3d3b145c0c587aa30376bd44351e1b67d
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-26 11:15:55 +0300

    Commit

--- ЛР 2/p2-6-40.rb
+++ ЛР 2/p2-6-40.rb
@@ -1,8 +1,28 @@
-puts "Введите элементы целочисленного массива (пустая строка завершает ввод):"
-a = []
-input = STDIN.gets.chomp
-while input != ""
-    a.concat(input.split.map{ |i| Integer(i)})
-    input = STDIN.gets.chomp
+def main
+    puts "Введите количество элементов"
+    n = Integer STDIN.gets.chomp
+    puts "Введите список"
+    list = []
+    for i in 0...n
+        item = Integer STDIN.gets.chomp
+        list[i] = item
+    end
+    max = list[0]
+    for i in 0...n
+        if max < list[i]
+            max = list[i]
+        end
+    end
+    min = max + 1
+    for i in 0...n
+        if (min > list[i]) && (list[i] % 2 == 0)
+            min = list[i]
+        end
+    end
+    if min == max + 1
+        print "Такого нет"
+    else
+        print min
+    end
 end
-print (a.filter { |i| i.even?}).min
+main
