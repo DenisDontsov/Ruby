95f7e964996169573784f19d4676f16efe670d2e
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-26 19:00:10 +0300

    Commit

--- ЛР 2/p2-6-52.rb
+++ ЛР 2/p2-6-52.rb
@@ -1,15 +1,48 @@
-require 'prime'
-puts "Введите число:"
-n = Integer STDIN.gets.chomp
-a = []
-Prime.each(n) do |prime|
-    if n % prime == 0
-        l = (Math.log(n, prime)).round
-        if prime**l == n
-            l.times {a << prime}
-        else
-            a << prime
+def prime number
+    a = []
+    for i in 0...number
+        a[i] = true
+    end
+    i = 2
+    while i*i <= number
+        if a[i] == true
+            j = i*i
+            while j <= number
+                a[j] = false
+                j += i
+            end 
+        end
+        i += 1
+    end
+    b = []
+    for i in 2...number
+        if a[i] == true
+            b << i
         end
     end
+    b
 end
-print a
+
+def main
+    puts "Введите число:"
+    n = Integer STDIN.gets.chomp
+    list = []
+    primes_list = prime(n)
+    for i in primes_list
+        if n % i == 0
+            counter = 1
+            j = i
+            while j < n
+                j*=i
+                counter += 1
+            end
+            if j == n
+                counter.times {list << i}
+            else
+                list << i
+            end
+        end
+    end
+    print list
+end
+main
