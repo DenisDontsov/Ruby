2290db641516efacdf1b7330429ac42ef6448ec3
DenisDontsov <denisdontsov21012001@gmail.com>
2021-03-27 13:20:28 +0300

    Commit

--- ЛР 2/p2-12.rb
+++ ЛР 2/p2-12.rb
@@ -5,6 +5,8 @@ def readFromFile(filePath)
     file = File.new(filePath, "r:UTF-8")
     return file.readlines
 end
-
+if ARGV.length < 1
+	abort "Введите путь ко входному файлу в качестве аргумента программы"
+end
 lines = readFromFile(ARGV[0])
 puts lines.sort_by {|line| line.length}
